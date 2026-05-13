c300ec47c91ae125b79942f568b672319630f234
Bhagyashri Vyawhare <150170466+bhagyashrivyawhare@users.noreply.github.com>
2026-05-13 11:31:25 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+// OpenClass Security Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAE729BAWQ3z22VviTKTCleBX0IstMkWfY",
+  authDomain: "openclass-e814a.firebaseapp.com",
+  projectId: "openclass-e814a",
+  storageBucket: "openclass-e814a.firebasestorage.app",
+  messagingSenderId: "955552487309",
+  appId: "1:955552487309:web:1ab0dd042529032ad20fc4",
+  measurementId: "G-JESQ4M5X6T"
+};
+
+export const emailConfig = {
+    serviceID: "service_djr1n0f",
+    templateID: "template_tiqnb53",
+    publicKey: "P88qoYcr5CHYVNAPw"
+};
+
+export const aiConfig = {
+    geminiKey: "AIzaSyCcHUvzU3TiORjiu7YgXU41I87L2GPrStk"
+};
